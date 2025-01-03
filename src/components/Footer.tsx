09cfdbda997ca52eda73ec64547bc09bdb8b2659
1stcodekami <mkkhurshid33@gmail.com>
2025-01-03 17:33:33 +0500

    Mile2 web Tailwind CSS

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 text-center">
-      <p>&copy; 2024 Watch World. All Rights Reserved.</p>
+      <p>&copy; 2024 Piece of time. All Rights Reserved.</p>
     </footer>
   );
 };
